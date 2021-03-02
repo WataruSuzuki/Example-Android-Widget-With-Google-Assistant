b67bfc766ee7a4b42092416018a2177dfc4a28c6
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-03-02 22:10:00 +0900

    Added "rememberNavController"

---
+++ app/src/main/java/com/example/samplegoogleassistant/Screens.kt
@@ -0,0 +1,9 @@
+package com.example.samplegoogleassistant
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+}
