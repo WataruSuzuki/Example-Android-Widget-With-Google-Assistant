8c1f665cadf9ae8a33f02b323c0fc401a25ba452
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-03-12 22:10:00 +0900

    Added another activity for trying app action

---
+++ app/src/main/java/com/example/samplegoogleassistant/DummyFeatureActivity.kt
@@ -0,0 +1,68 @@
+package com.example.samplegoogleassistant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.samplegoogleassistant.ui.theme.SampleGoogleAssistantTheme
+
+class DummyFeatureActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleGoogleAssistantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    NewsStory()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NewsStory() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(16.dp))
+
+            Text("A dummy for OPEN_APP_FEATURE.",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis)
+            Text("See also https://developers.google.com/assistant/app/reference/built-in-intents/common/open-app-feature",
+                style = typography.body2)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDummyFeature() {
+    NewsStory()
+}
